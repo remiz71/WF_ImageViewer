1b5bb76e5d86e47d00b06a8c3c9daf2ae7637d46
Remi_wr91 <Remi_wr91@itstepru.onmicrosoft.com>
2022-06-05 12:20:29 +0300

    done

--- WF_Image/Form1.cs
+++ WF_Image/Form1.cs
@@ -10,69 +10,104 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Threading;
+using Timer = System.Windows.Forms.Timer;
 
 namespace WF_Image
 {
     public partial class Form1 : Form
     {
+        Timer timer = new Timer();
         List<Image> _imageList = new List<Image>();
         int counter =0;
         bool isPlay = false;
         public Form1()
         {
             InitializeComponent();
+            timer.Interval = 500;
+            timer.Tick += _next_Click;
         }
 
         private void _openf_Click(object sender, EventArgs e)
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
             {
-                string[] _filepaths = Directory.GetFiles(folderBrowserDialog1.SelectedPath, "*.jpg");
-                foreach(var items in _filepaths)
+                timer.Stop();
+                if(_imageList.Count!=0)
                 {
-                    _imageList.Add(new Bitmap(items));
+                    foreach (var item in _imageList)
+                    {
+                        item.Dispose();
+                    }
+                    _imageList.Clear();
                 }
+                DirectoryInfo di = new DirectoryInfo(folderBrowserDialog1.SelectedPath);
+                IEnumerable<FileInfo> alf = di.EnumerateFiles();
+                foreach(var items in alf)
+                {
+                    string ex = Path.GetExtension(items.FullName);
+                    if(ex==".bmp" || ex == ".jpg" || ex == ".png")
+                    {
+                    _imageList.Add(new Bitmap(items.FullName));
+
+                    }
+                }
+                counter = 0;
                 pictureBox1.Image = _imageList[counter];
                 
-                label1.Text = label1.Text = $"{counter + 1} / {_imageList.Count}"; ;
+                label1.Text = $"{counter + 1} / {_imageList.Count}"; ;
             }
         }
 
         private void _next_Click(object sender, EventArgs e)
         {
-            if (counter < _imageList.Count - 1) counter++;
-            else counter = 0;
-            label1.Text = $"{counter + 1} / {_imageList.Count}";
-            pictureBox1.Image = _imageList[counter];
+            NextPicture();
             
+        }
+
+        private void NextPicture()
+        {
+            if (_imageList.Count == 0)
+            {
+                return;
+            }
+            counter++;
+            if (counter >= _imageList.Count)
+                counter = 0;
+            pictureBox1.Image = _imageList[counter];
+            label1.Text = $"{counter + 1} / {_imageList.Count}";
         }
 
         private void _prev_Click(object sender, EventArgs e)
         {
-            if (counter > 0) counter--;
-            else counter = _imageList.Count - 1;
-            label1.Text = $"{counter + 1} / {_imageList.Count}";
+            if (_imageList.Count == 0)
+            {
+                return;
+            }
+            counter--;
+            if (counter < 0)
+                counter = _imageList.Count-1;
             pictureBox1.Image = _imageList[counter];
-            
+            label1.Text = $"{counter + 1} / {_imageList.Count}";
+
         }
 
         private void _start_Click(object sender, EventArgs e)
         {
-
-            do
+            if (_imageList.Count!=0)
             {
-                pictureBox1.Image = _imageList[counter];
-                counter++;
-                if (counter > _imageList.Count - 1)
-                    counter = 0;
-                Thread.Sleep(200);
-            } while (isPlay);
+            timer.Start();
+
+            }
+            else
+            {
+                MessageBox.Show("Не выбрана папка");
+            }
 
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            isPlay = false;
+            timer.Stop();
         }
     }
 }
